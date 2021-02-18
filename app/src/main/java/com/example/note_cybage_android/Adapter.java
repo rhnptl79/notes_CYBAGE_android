cff1ab35ef573ffbb1d5b446b2291d648b5778fe
Rohan <rhnptl79@gmail.ccom>
2021-02-18 17:46:33 +0530

    Notes 9

--- app/src/main/java/com/example/note_cybage_android/Adapter.java
+++ app/src/main/java/com/example/note_cybage_android/Adapter.java
@@ -2,6 +2,7 @@ package com.example.note_cybage_android;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -37,6 +38,18 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         holder.title.setText(notesList.get(position).getTitle());
         holder.description.setText(notesList.get(position).getDescription());
+
+        holder.layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context,UpdateNotesActivity.class);
+                intent.putExtra("title",notesList.get(position).getTitle());
+                intent.putExtra("description",notesList.get(position).getDescription());
+                intent.putExtra("id",notesList.get(position).getId());
+
+                activity.startActivity(intent);
+            }
+        });
     }
 
     @Override
