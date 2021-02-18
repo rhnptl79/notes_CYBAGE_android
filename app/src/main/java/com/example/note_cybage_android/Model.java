1138675f84458d9edb9742b33396d9de0d712acc
Rohan <rhnptl79@gmail.ccom>
2021-02-18 16:17:52 +0530

    Notes 6

--- app/src/main/java/com/example/note_cybage_android/Model.java
+++ app/src/main/java/com/example/note_cybage_android/Model.java
@@ -1,4 +1,38 @@
 package com.example.note_cybage_android;
 
 public class Model {
+
+    String title;
+    String description;
+    String id;
+
+    public Model(String title, String description, String id) {
+        this.title = title;
+        this.description = description;
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
