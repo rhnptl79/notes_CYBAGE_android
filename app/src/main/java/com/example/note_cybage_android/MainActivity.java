2d250121a16947f59c1a3bc7f601a7c1d4a47911
Rohan <rhnptl79@gmail.ccom>
2021-02-18 15:16:46 +0530

    Notes 3

--- app/src/main/java/com/example/note_cybage_android/MainActivity.java
+++ app/src/main/java/com/example/note_cybage_android/MainActivity.java
@@ -3,13 +3,20 @@ package com.example.note_cybage_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
-    //
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+        
     }
 }
